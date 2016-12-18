ba83a30638b3d3670016ae360f0cbccffa8d41b9
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-12-18 17:58:18 +0000

    Fixing button style and cleaning up code

--- mail_handler.php
+++ mail_handler.php
@@ -15,24 +15,11 @@ if(isset($_POST['submit'])){
 
     if ($_POST['submit']) {
         if (mail ($to, $subject, $message, $from)) { 
-            echo $name."<br>";
-            echo $from."<br>";
-            echo $phone."<br>";
-            echo $subject."<br>";
-            echo $message."<br>";
             echo '<p>Your message has been sent!</p>';
         } else { 
-            echo $name."<br>";
-            echo $from."<br>";
-            echo $phone."<br>";
-            echo $subject."<br>";
-            echo $message."<br>";
             echo '<p>Something went wrong, go back and try again!</p>'; 
         }
     }
-    //echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
-    }
+  }
 ?>
 
